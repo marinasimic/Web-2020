5c737de760c609fe3aa42867c4232bda11cd5ca4
Marina Simic <simic.marina@outlook.com>
2020-04-28 01:51:08 +0200

    Removed unnecessary files. Added UserDAO and UserService.

---
+++ src/services/UserService.java
@@ -0,0 +1,64 @@
+package services;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import beans.User;
+import dao.UserDAO;
+
+@Path("")
+public class UserService {
+	
+	@Context
+	ServletContext ctx;
+	
+	public UserService(){}
+	
+	@PostConstruct
+	public void init(){
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		if (ctx.getAttribute("userDAO") == null){
+		    String contextPath = ctx.getRealPath("");
+		    ctx.setAttribute("userDAO", new UserDAO(contextPath));
+		}
+	}
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response login(User user, @Context HttpServletRequest request) {
+		UserDAO userDao = (UserDAO) ctx.getAttribute("userDAO");
+		User loggedUser = userDao.find(user.getUsername(), user.getPassword());
+		if (loggedUser != null) {
+			return Response.status(400).entity("Invalid username and/or password").build();
+		}
+		request.getSession().setAttribute("user", loggedUser);
+		return Response.status(200).build();
+	}
+	
+	@POST
+	@Path("/logout")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void logout(@Context HttpServletRequest request) {
+		request.getSession().invalidate();
+	}
+	
+	@GET
+	@Path("/currentUser")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User login(@Context HttpServletRequest request) {
+		return (User) request.getSession().getAttribute("user");
+	}
+}
